85eb1af4a6c37632009af5e22a792203e389733a
isaac <isaacfeglarfiacrememiniedou@esp.sn>
2025-06-14 18:08:12 +0000

    amélioration pour la visualisation

--- src/components/simulation/SimulationView.tsx
+++ src/components/simulation/SimulationView.tsx
@@ -136,7 +136,7 @@ const SimulationView: React.FC<{isActive: boolean}> = ({isActive}) => {
       </div>
 
       {/* Zone de visualisation 3D */}
-      <div className="w-3/4">
+      <div className="w-3/4 h-screen">
         <Canvas camera={{ position: [0, 5, 10], fov: 75 }}>
           <ambientLight intensity={0.5} />
           <directionalLight position={[10, 10, 5]} intensity={1} />
